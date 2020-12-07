4e0fce8a3e95e194287bb670d209fa31789ecd52
zhuangzhuang-huang <cau1403090523@gmail.com>
2020-12-08 00:34:47 +0800

    create react project and create homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">电脑</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">水杯</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">手机</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMEN</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+            <div className="content">
+                <h1 className="title">MAN</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+
+        </div>
+    </div>
+
+);
+
+export default HomePage;
