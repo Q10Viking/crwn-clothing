7443f446eb0c5f3e8eea0b3fa8cae5c6e1aacce8
zhuangzhuang-huang <cau1403090523@gmail.com>
2020-12-08 14:57:02 +0800

    完善

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Route,Switch} from 'react-router-dom';
-
 import ShopPage from './pages/shoppage/shop-page.component';
 
 function App() {
