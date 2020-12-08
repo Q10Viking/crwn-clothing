a39362797cf093299ee5c788c13fb1eb0d16415a
zhuangzhuang-huang <cau1403090523@gmail.com>
2020-12-08 14:30:03 +0800

    添加shop page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Route,Switch} from 'react-router-dom';
 
-
-const Hats = () => (
-  <div><h1>HATS</h1></div>
-);
-
+import ShopPage from './pages/shoppage/shop-page.component';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={Hats} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shoppage/shop-page.component.jsx
@@ -0,0 +1,9 @@
+import './shop-page.styles.scss';
+
+const ShopPage = () => (
+    <div>
+        SHOP PAGE
+    </div>
+);
+
+export default ShopPage;
