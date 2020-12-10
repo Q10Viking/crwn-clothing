44403bdd64ebece66ef5fea88d356848e00c3111
zhuangzhuang-huang <cau1403090523@gmail.com>
2020-12-10 09:18:53 +0800

    show cart item

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,16 @@
+import './cart-item.styles.scss';
+
+const CartItem = ({item:{name,price,imageUrl,quantity}}) => (
+    <div className="cart-item">
+        <img src={imageUrl} alt="goods imgage"/>
+
+        <div className="item-details">
+            <span className="name">{name}</span>
+            <span className="price">
+                {quantity} X {price}
+            </span>
+        </div>
+    </div>
+);
+
+export default CartItem;
