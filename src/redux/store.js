842e9af9c49e5824d4d70b482dc8f42d92726049
zhuangzhuang-huang <cau1403090523@gmail.com>
2020-12-10 20:00:57 +0800

    finish redux store persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,27 @@
 // import {createStore,applyMiddleware} from 'redux';
 import {createStore} from 'redux';
+import {persistStore,persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';    // defaults to localStorage for web
 import rootReducer from './root-reducer';
+
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'] // only navigation will be persisted
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+
 // import logger from 'redux-logger';
 const reduxdebug = (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 // const middlewares = [logger];
 // const store = createStore(rootReducer,applyMiddleware(...middlewares));
-const store = createStore(rootReducer,reduxdebug);
-export default store;
+const store = createStore(persistedReducer,reduxdebug);
+const persistor = persistStore(store);
+
+export{
+    store,
+    persistor
+}
